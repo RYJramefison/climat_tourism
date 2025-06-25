5e06f0a4c9506d8f7466321d677ffebfe60d8982
juninho <hei.juninho.2@gmail.com>
2025-06-25 14:54:09 +0300

    feat: added multi-city execution support in weather DAG

--- dags/etl_climat_dag.py
+++ dags/etl_climat_dag.py
@@ -9,32 +9,47 @@ from scripts.extract import get_city_coordinates, get_weather_forecast
 from scripts.transform import clean_weather_data
 from scripts.load import save_to_csv
 
-CITY = "Antananarivo"
+CITIES = ["Antananarivo", "Paris", "Tokyo"]
 API_KEY = Variable.get("API_KEY_climat_tourisme")
 ## API_KEY = "{{ var.value.API_KEY_climat_tourisme }}"
 
 with DAG("etl_climat_dag", start_date=datetime(2024, 1, 1), schedule="@daily", catchup=False) as dag:
+    previous_last_task = None
+    
+    for CITY in CITIES:
+        def extract_coords(CITY):
+            def task():
+                lat, lon = get_city_coordinates(CITY, API_KEY)
+                return {"lat": lat, "lon": lon}
+            return task
 
-    def extract_coords():
-        lat, lon = get_city_coordinates(CITY, API_KEY)
-        return {"lat": lat, "lon": lon}
+        def extract_weather(CITY):
+            def task():
+                return get_weather_forecast(CITY, API_KEY)
+            return task
 
-    def extract_weather():
-        return get_weather_forecast(CITY, API_KEY)
+        def transform(CITY):
+            def task():
+                raw = get_weather_forecast(CITY, API_KEY)
+                df = clean_weather_data(raw)
+                return df
+            return task
 
-    def transform():
-        raw = get_weather_forecast(CITY, API_KEY)
-        df = clean_weather_data(raw)
-        return df
+        def load(CITY):
+            def task():
+                raw = get_weather_forecast(CITY, API_KEY)
+                df = clean_weather_data(raw)
+                save_to_csv(df,"airflow/dags/climat_tourisme/data/weather_"+CITY+".csv")
+            return task
+    
+        t1 = PythonOperator(task_id=f"extract_city_{CITY.lower()}", python_callable=extract_coords(CITY))
+        t2 = PythonOperator(task_id=f"extract_data_{CITY.lower()}", python_callable=extract_weather(CITY))
+        t3 = PythonOperator(task_id=f"clean_data_{CITY.lower()}", python_callable=transform(CITY))
+        t4 = PythonOperator(task_id=f"save_to_csv_{CITY.lower()}", python_callable=load(CITY))
 
-    def load():
-        raw = get_weather_forecast(CITY, API_KEY)
-        df = clean_weather_data(raw)
-        save_to_csv(df,"airflow/dags/climat_tourisme/data/weather_"+CITY+".csv")
+        t1 >> t2 >> t3 >> t4
+        
+        if previous_last_task:
+            previous_last_task >> t1  # la dernière tâche précédente vers le t1 de cette ville
 
-    t1 = PythonOperator(task_id="extract_city_coords", python_callable=extract_coords)
-    t2 = PythonOperator(task_id="extract_weather_data", python_callable=extract_weather)
-    t3 = PythonOperator(task_id="clean_weather_data", python_callable=transform)
-    t4 = PythonOperator(task_id="save_to_csv", python_callable=load)
-
-    t1 >> t2 >> t3 >> t4
+        previous_last_task = t4 
